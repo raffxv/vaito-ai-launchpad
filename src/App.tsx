2ac506dfc49a81dce01c25d63f65343d532edd3e
New Addict <121753715+raffxv@users.noreply.github.com>
2025-11-09 14:31:54 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import WalletContextProvider from "@/lib/WalletContextProvider";
 
 // 🔹 Solana wallet dependencies
 import {
